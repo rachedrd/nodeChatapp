b137e1fe10fc99575c120188d7fad7142a7d6d4d
rachedrd <nemrrached@hotmail.com>
2017-04-17 14:55:30 +0100

    fixing fb app

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var express = require('express'),
 	rooms = [],
 	connectMongo = require('connect-mongo')(session);
 	
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 3000));
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('hogan-express'));
 app.set('view engine', 'html');
